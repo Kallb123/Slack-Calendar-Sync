767fb7173a94892f99969d65b24f0d75b6aea4b5
Kallum Burgin <Kallb123@users.noreply.github.com>
2024-12-16 12:20:02 +0000

    Filter event list with only accepted events

--- googleController.ts
+++ googleController.ts
@@ -81,8 +81,20 @@ async function getCurrentGenericEvent(gAuth: Credentials, eventType: string): Pr
   if (!events || events.length === 0) {
     return;
   }
-  events.sort(eventSort);
-  return events[0];
+  // Filter the events so that it is only ones related to the user
+  let acceptedEvents = events.filter(event => {
+    // If the event has no attendees, then it is likely OOO or workingLocation, so let it through
+    if (!event.attendees || event.attendees.length === 0) return true;
+    // Locate the relevant user in the list of attendees, not entirely sure this works
+    var thisAttendee = event.attendees?.find(attendee => attendee.self === true);
+    // Allow events that are accepted or tentatively accepted
+    return thisAttendee?.responseStatus === 'accepted' || thisAttendee?.responseStatus === 'tentative';
+  });
+  if (!acceptedEvents || acceptedEvents.length === 0) {
+    return;
+  }
+  acceptedEvents.sort(eventSort);
+  return acceptedEvents[0];
 }
 
 // Gets the first (assuming today's) working location for the authenticated user
